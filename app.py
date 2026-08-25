03307395f2dc4fe5659f7b5ea13abb0cb693ea1d
olaniyihadrat4u2c-boop <olaniyihadrat4u2c@gmail.com>
2026-08-25 16:17:00 +0300

    you are gonna know what it does with the comments

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 #contains the app
 from flask import Flask
 
-
+#um random thing i think it allows the thing to like route and bring you the thing i dont really know 
 app = Flask(__name__)
 
+#these are the different routes
 @app.route("/")
 def home():
-    return "yokoso wathisno no soul society"
+    return "orewa monkey d luffy"
+
+@app.route("/about")
+def about():
+    return "kaitoku shizen"
+
+@app.route("/contact")
+def contact():
+    return "orewa dev_cocalee i'l become the king of coders"
